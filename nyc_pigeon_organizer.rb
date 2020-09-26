c07e959262df48b71bfb7c1115c5d7ab422a24f3
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-26 18:48:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,11 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   final_results =data.each_with_object({}) do |(key, value), new_hash|
     value.each do |inner_key, names|
+  end
     names.each do |pigeon_name| = {}
   end
       if !new_hash[pigeon_name][key]
+    end
          !new_hash[pigeon_name][key] = []
        end
     new_hash[pigeon_name][key].push(inner_key.to_s)
