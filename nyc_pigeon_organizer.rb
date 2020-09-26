6ad921279951efea1696ca87a1c69537a8d3160b
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-26 19:12:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,4 +5,15 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |life_info, names|
       names.each do |name|
+    end
+    if !new_hash[name]
+      new_hash[name] = {}
+    end
+    if !new_hash[name][key]
+      new_hash[nam][key] = []
+    end
+    new_hash[name][key] << (life_info.to_s)
+  end
+end
+new_hash
 end
